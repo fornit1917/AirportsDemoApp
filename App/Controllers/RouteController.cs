35437adc06f5f2cc92a0dc27c4c849f11138d34e
fornit1917 <fornit1917@gmail.com>
2018-11-13 20:34:12 +0300

    Initial project commit

---
+++ App/Controllers/RouteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AirportsDemo.App.Models;
+
+namespace AirportsDemo.App.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RouteController : ControllerBase
+	{
+		[HttpGet("search")]
+		public Task<Flight[]> Search(string srcAirport, string destAirport)
+		{
+			var testData = new Flight[2] { new Flight("Airline_1", srcAirport, "SomeAirport"), new Flight("Airline_2", "SomeAirport", destAirport) };
+			return Task.FromResult(testData);
+		}
+	}
+}
---
+++ App/Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirportsDemo.App.Models
+{
+	public class Flight
+	{
+		public string Airline { get; private set; }
+		public string SrcAirport { get; private set; }
+		public string DestAirport { get; private set; }
+
+		public Flight(string airline, string srcAirport, string destAirport)
+		{
+			Airline = airline;
+			SrcAirport = srcAirport;
+			DestAirport = destAirport;
+		}
+	}
+}
